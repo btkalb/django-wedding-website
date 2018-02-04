d3440a44da1542952ae977e7536d4746e445ce34
Brian Kalb <bkalb@knackworks.com>
2018-02-04 17:41:05 -0500

    add rsvp and fix mobile

--- bigday/settings.py
+++ bigday/settings.py
@@ -58,6 +58,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join('bigday', 'templates'),
+            os.path.join('templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
